f0ccb8fe2b653ac2c5ba15e275bf631b0a40d10b
NurAzliYT <135945845+NurAzliYT@users.noreply.github.com>
2023-10-06 16:30:21 +0700

    Add files via upload

--- src/NurAzliYT/SHOP/events/PlayerSuccessBuyEvent.php
+++ src/NurAzliYT/SHOP/events/PlayerSuccessBuyEvent.php
@@ -1,1 +1,23 @@
+<?php
 
+namespace SafiraaCute\Shop\events;
+
+use pocketmine\event\player\PlayerEvent;
+use pocketmine\player\Player;
+
+class PlayerSuccessBuyEvent extends PlayerEvent {
+
+    private array $data;
+
+    public function __construct(array $data, Player $player) {
+        $this->data = $data;
+        $this->player = $player;
+    }
+    
+    /**
+     * @return array
+     */
+    public function getItem(): array {
+        return $this->data;
+    }
+}
